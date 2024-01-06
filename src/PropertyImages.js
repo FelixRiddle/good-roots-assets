1e9c9f463053c8307e9358a874a5e92868d434fd
Felix <fedecraft3312@gmail.com>
2024-01-06 20:42:12 -0300

    Images absolute path

--- src/PropertyImages.js
+++ src/PropertyImages.js
@@ -2,6 +2,37 @@ import fs from "node:fs";
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+/**
+ * Property images path
+ * 
+ * @returns {string}
+ */
+function propertyImagesPath() {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const propertyImages = `${__dirname}/../images/properties`;
+    return propertyImages;
+}
+
+/**
+ * Images to absolute path
+ * 
+ * @param {Array} images
+ * @param {string} houseName The house name(the folder containing the images)  
+ * @returns 
+ */
+function imagesToAbsolutePath(images, houseName) {
+    const propPath = propertyImagesPath();
+    const imagesPath = [];
+    for(const image of images) {
+        const imagePath = path.resolve(`${propPath}/${houseName}/${image}`);
+        imagesPath.push(imagePath);
+    }
+    
+    console.log(`Images path: `, imagesPath);
+    return imagesPath;
+}
+
 export default class PropertyImages {
     constructor() {
         
@@ -13,17 +44,20 @@ export default class PropertyImages {
     getAll() {
         let result = {};
         
-        const __filename = fileURLToPath(import.meta.url);
-        const __dirname = path.dirname(__filename);
-        const propertyImages = `${__dirname}/../images/properties`;
+        const propertyImages = propertyImagesPath();
         
         const folders = fs.readdirSync(propertyImages);
         
         for(let entry of folders) {
-            const images = fs.readdirSync(`${propertyImages}/${entry}`);
-            result[entry] = images;
+            const entryPath = path.resolve(`${propertyImages}/${entry}`);
+            
+            const images = fs.readdirSync(entryPath);
+            const imagesAbsPath = imagesToAbsolutePath(images, entry);
+            
+            result[entry] = imagesAbsPath;
         }
         
+        console.log(`Result: `, result);
         return result;
     }
 }
