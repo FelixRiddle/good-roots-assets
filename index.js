e3ceb6de402179b72d19e2209ab4dea77d46d84d
Felix <fedecraft3312@gmail.com>
2024-01-06 21:44:50 -0300

    Function to create properties data

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import PropertyImages from "./src/PropertyImages.js";
+import { seedProperties } from "./src/seed/property.js";
 
 const propertyImages = new PropertyImages();
 propertyImages.getAll();
+
+seedProperties();
